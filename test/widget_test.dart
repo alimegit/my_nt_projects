25e42482dca01e97e39a13faee87b18174703c51
Ali <alisherurinov@68@gmail.com>
2024-02-12 22:14:21 +0500

    Default

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_nt_ten/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_nt_projects/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
